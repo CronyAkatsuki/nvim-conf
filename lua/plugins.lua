d932dc535c64283059b190557d4520c1431721ed
cronyakatsuki <crolinuxgamer@gmail.com>
2022-04-02 15:51:28 +0200

    Removed unnedded plugins, changed color scheme to doom-one, added indentline support.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -11,89 +11,121 @@ end
 vim.cmd "autocmd BufWritePost plugins.lua PackerCompile" -- Auto compile when there are changes in plugins.lua
 
 return require('packer').startup(function()
-		-- plugin manager
-		use 'wbthomason/packer.nvim'
+	-- plugin manager
+	use 'wbthomason/packer.nvim'
 
-		-- file explorer
-		use 'kyazdani42/nvim-tree.lua'
+	-- file explorer
+	use 'kyazdani42/nvim-tree.lua'
 
-		-- lsp support
-		use 'neovim/nvim-lspconfig'
+	-- lsp support
+	use 'neovim/nvim-lspconfig'
 
-		-- rust tools
-		use 'simrat39/rust-tools.nvim'
+	-- rust tools
+	use 'simrat39/rust-tools.nvim'
 
-		-- telescope
-		use "nvim-lua/popup.nvim"
-		use 'nvim-lua/plenary.nvim'
-		use "nvim-telescope/telescope.nvim"
-		use "glepnir/lspsaga.nvim"
+	-- telescope
+	use "nvim-lua/popup.nvim"
+	use 'nvim-lua/plenary.nvim'
+	use "nvim-telescope/telescope.nvim"
+	use "glepnir/lspsaga.nvim"
 
-		-- completion support
-		use 'hrsh7th/cmp-nvim-lsp'
-		use 'hrsh7th/cmp-buffer'
-		use 'hrsh7th/cmp-path'
-		use 'hrsh7th/cmp-cmdline'
-		use 'ray-x/cmp-treesitter'
-		use 'hrsh7th/nvim-cmp'
-		use 'hrsh7th/cmp-nvim-lua'
+	-- completion support
+	use 'hrsh7th/cmp-nvim-lsp'
+	use 'hrsh7th/cmp-buffer'
+	use 'hrsh7th/cmp-path'
+	use 'hrsh7th/cmp-cmdline'
+	use 'ray-x/cmp-treesitter'
+	use 'hrsh7th/nvim-cmp'
+	use 'hrsh7th/cmp-nvim-lua'
 
-		-- snippets
-		use 'dcampos/nvim-snippy'
-		use 'dcampos/cmp-snippy'
+	-- snippets
+	use 'dcampos/nvim-snippy'
+	use 'dcampos/cmp-snippy'
 
-		-- kind Icons
-		use 'onsails/lspkind-nvim'
+	-- kind Icons
+	use 'onsails/lspkind-nvim'
 
-		-- debugging
-		use 'mfussenegger/nvim-dap'
-		use 'theHamsta/nvim-dap-virtual-text'
-		use 'mfussenegger/nvim-dap-python'
+	-- debugging
+	use 'mfussenegger/nvim-dap'
+	use 'theHamsta/nvim-dap-virtual-text'
+	use 'mfussenegger/nvim-dap-python'
 
-		-- tree sitter
-		use "nvim-treesitter/nvim-treesitter"
+	-- tree sitter
+	use "nvim-treesitter/nvim-treesitter"
 
-		-- autopairs
-		use 'windwp/nvim-autopairs'
+	-- autopairs
+	use 'windwp/nvim-autopairs'
 
-		-- Icons
-		use "kyazdani42/nvim-web-devicons"
+	-- Icons
+	use "kyazdani42/nvim-web-devicons"
 
-		-- easy commenting
-		use "terrortylor/nvim-comment"
+	-- easy commenting
+	use "terrortylor/nvim-comment"
 
-		-- nice line
-		use 'nvim-lualine/lualine.nvim'
+	-- nice line
+	use 'nvim-lualine/lualine.nvim'
 
-		-- git signs
-		use 'lewis6991/gitsigns.nvim'
+	-- git signs
+	use 'lewis6991/gitsigns.nvim'
 
-		-- nice start page
-		use 'glepnir/dashboard-nvim'
-		use 'kevinhwang91/nvim-bqf'
+	-- nice start page
+	use 'glepnir/dashboard-nvim'
+	use 'kevinhwang91/nvim-bqf'
 
-		-- colors are mine
-		use 'norcalli/nvim-colorizer.lua'
+	-- colors are mine
+	use 'norcalli/nvim-colorizer.lua'
 
-		-- theme
-		use { "ellisonleao/gruvbox.nvim" }
-		-- use 'Mofiqul/dracula.nvim'
+	-- theme
+	use({
+    'NTBBloodbath/doom-one.nvim',
+    config = function()
+        require('doom-one').setup({
+            cursor_coloring = true,
+            terminal_colors = true,
+            italic_comments = true,
+            enable_treesitter = true,
+            transparent_background = false,
+            pumblend = {
+                enable = true,
+                transparency_amount = 20,
+            },
+            plugins_integrations = {
+                neorg = false,
+                barbar = false,
+                bufferline = true,
+                gitgutter = false,
+                gitsigns = true,
+                telescope = true,
+                neogit = true,
+                nvim_tree = false,
+                dashboard = true,
+                startify = false,
+                whichkey = false,
+                indent_blankline = true,
+                vim_illuminate = false,
+                lspsaga = true,
+			},
+        })
+		end,
+	})
 
-		-- buffer line
-		use {'akinsho/bufferline.nvim', requires = 'kyazdani42/nvim-web-devicons'}
+	-- use { "ellisonleao/gruvbox.nvim" }
 
-		-- terminal toggle
-		use {"akinsho/toggleterm.nvim"}
+	-- buffer line
+	use {'akinsho/bufferline.nvim', requires = 'kyazdani42/nvim-web-devicons'}
 
-		-- rainbow parantheses
-		use "p00f/nvim-ts-rainbow"
+	-- terminal toggle
+	use {"akinsho/toggleterm.nvim"}
 
-		-- automatically create directory on save
-		use 'jghauser/mkdir.nvim'
+	-- rainbow parantheses
+	use "p00f/nvim-ts-rainbow"
 
-		-- spellchecker
-		use 'lewis6991/spellsitter.nvim'
+	-- automatically create directory on save
+	use 'jghauser/mkdir.nvim'
 
-		-- better search
-		use {'kevinhwang91/nvim-hlslens'}
+	-- better search
+	use {'kevinhwang91/nvim-hlslens'}
+
+	-- indentline
+	use 'lukas-reineke/indent-blankline.nvim'
 end)
