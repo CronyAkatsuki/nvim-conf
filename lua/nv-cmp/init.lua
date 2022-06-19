3c49870114424f38492e33668278f4a8204a9d9b
cronyakatsuki <crolinuxgamer@gmail.com>
2022-06-19 19:53:51 +0200

    Removed feature that breaks cmp.

--- lua/nv-cmp/init.lua
+++ lua/nv-cmp/init.lua
@@ -4,7 +4,7 @@ local cmp = require("cmp")
 local lspkind = require("lspkind")
 local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
-cmp_autopairs.lisp[#cmp_autopairs.lisp + 1] = "racket"
+--> cmp_autopairs.lisp[#cmp_autopairs.lisp + 1] = "racket"
 
 cmp.setup({
 	formatting = {
