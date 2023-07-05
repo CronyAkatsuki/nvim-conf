73e29175efb961c7d45f8a6e5c59d8ea375290d7
CronyAkatsuki <crony@cronyakatsuki.xyz>
2023-07-05 19:35:21 +0200

    Update catppuccin settings.

--- lua/crony/plugins/catppuccin.lua
+++ lua/crony/plugins/catppuccin.lua
@@ -3,6 +3,39 @@ return {
 	name = "catppuccin",
 	priority = 1000,
 	opts = {
-		flavour = "frappe"
+		flavour = "frappe",
+		integrations = {
+			alpha = true,
+			bufferline = true,
+			cmp = true,
+			gitsigns = true,
+			harpoon = true,
+			indent_blankline = {
+				enabled = true,
+				colored_indent_levels = false,
+			},
+			markdown = true,
+			mason = true,
+			mini = true,
+			neotree = true,
+			dap = {
+				enabled = true,
+				enable_ui = true, -- enable nvim-dap-ui
+			},
+			native_lsp = {
+				enabled = true,
+				underlines = {
+					errors = { "undercurl" },
+					hints = { "undercurl" },
+					warnings = { "undercurl" },
+					information = { "undercurl" },
+				},
+			},
+            notify = true,
+			telescope = true,
+            lsp_trouble = true,
+			treesitter = true,
+			which_key = true,
+		},
 	},
 }
