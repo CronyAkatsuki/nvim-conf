25ed8bec92a68fe231109a2a925e424271166658
CronyAkatsuki <crony@cronyakatsuki.xyz>
2023-07-05 19:36:45 +0200

    Add more remap's.

--- lua/crony/remap.lua
+++ lua/crony/remap.lua
@@ -1,22 +1,47 @@
+local key = vim.keymap.set
+
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Open NetRW" })
+key("n", "<leader>pv", vim.cmd.Ex, { desc = "Open NetRW" })
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selected text up" })
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selected text down" })
+key("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selected text up" })
+key("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selected text down" })
 
-vim.keymap.set("n", "J", "mzJ`z", { desc = "Move up next line with space in between" })
-vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Half page movement" })
-vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Half page movement" })
-vim.keymap.set("n", "n", "nzzzv", { desc = "Keep cursor in middle with search" })
-vim.keymap.set("n", "N", "Nzzzv", { desc = "Keep cursor in middle with search" })
+key("n", "J", "mzJ`z", { desc = "Move up next line with space in between" })
+key("n", "<C-d>", "<C-d>zz", { desc = "Half page movement" })
+key("n", "<C-u>", "<C-u>zz", { desc = "Half page movement" })
+key("n", "n", "nzzzv", { desc = "Keep cursor in middle with search" })
+key("n", "N", "Nzzzv", { desc = "Keep cursor in middle with search" })
 
 -- greatest remap ever
-vim.keymap.set("x", "<leader>p", [["_dP]], { desc = "Paste while keeping the registry" })
+key("x", "<leader>p", [["_dP]], { desc = "Paste while keeping the registry" })
 
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, { desc = "Format with lsp" })
+key("n", "<leader>f", vim.lsp.buf.format, { desc = "Format with lsp" })
 
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+key("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
-vim.keymap.set("n", "<leader><leader>", function()
+key("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
+
+-- Better window navigation
+key("n", "<C-h>", "<C-w>h")
+key("n", "<C-j>", "<C-w>j")
+key("n", "<C-k>", "<C-w>k")
+key("n", "<C-l>", "<C-w>l")
+
+-- Resize with arrows
+key("n", "<C-Up>", ":resize -2<CR>")
+key("n", "<C-Down>", ":resize +2<CR>")
+key("n", "<C-Left>", ":vertical resize -2<CR>")
+key("n", "<C-Right>", ":vertical resize +2<CR>")
+
+-- Navigate buffers
+key("n", "<S-l>", ":bnext<CR>")
+key("n", "<S-h>", ":bprevious<CR>")
+
+--> close tab
+key("n", "<Leader>qt", ":tabclose<CR>")
+
+--> better indenting
+key("v", "<", "<gv")
+key("v", ">", ">gv")
