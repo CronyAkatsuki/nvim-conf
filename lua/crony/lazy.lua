c143f1a92e201ab7f487ca90a784bacb070725bd
CronyAkatsuki <crony@cronyakatsuki.xyz>
2023-07-05 19:35:09 +0200

    Update lazy.nvim settings.

--- lua/crony/lazy.lua
+++ lua/crony/lazy.lua
@@ -1,17 +1,21 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("crony.plugins", {
+require("lazy").setup({
+    spec = {
+        { import = "crony.plugins"},
+        { import = "crony.plugins.lang.clang" },
+    },
 	defaults = { lazy = true },
 	install = { colorscheme = { "catppuccin" } },
 	checker = { enabled = true },
@@ -28,7 +32,7 @@ require("lazy").setup("crony.plugins", {
 				"fzf",
 				"zip",
 				"zipPlugin",
-			}
-		}
-	}
+			},
+		},
+	},
 })
