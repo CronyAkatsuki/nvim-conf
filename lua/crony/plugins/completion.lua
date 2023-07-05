a593f2ee439885f8235883a5be50fa50ebdb19b0
CronyAkatsuki <crony@cronyakatsuki.xyz>
2023-07-05 19:35:52 +0200

    Update completion plugins and settings.

--- lua/crony/plugins/completion.lua
+++ lua/crony/plugins/completion.lua
@@ -1,16 +1,16 @@
 return {
-    {
-        "L3MON4D3/LuaSnip",
-        dependencies = {
-            "rafamadriz/friendly-snippets",
-            config = function()
-                require("luasnip.loaders.from_vscode").lazy_load()
-            end,
-        },
-        opts = {
-            history = true,
-            delete_check_events = "TextChanged",
-        },
+	{
+		"L3MON4D3/LuaSnip",
+		dependencies = {
+			"rafamadriz/friendly-snippets",
+			config = function()
+				require("luasnip.loaders.from_vscode").lazy_load()
+			end,
+		},
+		opts = {
+			history = true,
+			delete_check_events = "TextChanged",
+		},
         -- stylua: ignore
         keys = {
             {
@@ -25,91 +25,148 @@ return {
             { "<tab>",   function() require("luasnip").jump(1) end,  mode = "s" },
             { "<s-tab>", function() require("luasnip").jump(-1) end, mode = { "i", "s" } },
         },
-    },
-    {
-        'hrsh7th/nvim-cmp',
-        event = 'InsertEnter',
-        dependencies = {
-            "hrsh7th/cmp-buffer",
-            "hrsh7th/cmp-path",
-            "saadparwaiz1/cmp_luasnip"
-        },
-        config = function()
-            local cmp = require('cmp')
-            local defaults = require('cmp.config.default')
-            local cmp_select_opts = { behavior = cmp.SelectBehavior.Select }
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		event = "VeryLazy",
+		dependencies = {
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-cmdline",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-nvim-lsp",
+			"saadparwaiz1/cmp_luasnip",
+		},
+		config = function()
+			local cmp = require("cmp")
+			local defaults = require("cmp.config.default")
+			local cmp_select_opts = { behavior = cmp.SelectBehavior.Select }
 
-            local cmp_config = {
-                completion = {
-                    completeopt = 'menu,menuone,noinsert'
-                },
-                snippet = {
-                    expand = function(args)
-                        require("luasnip").lsp_expand(args.body)
-                    end,
-                },
-                sources = {
-                    { name = 'nvim_lsp' },
-                    { name = 'luasnip' },
-                    { name = 'buffer' },
-                    { name = 'path' },
-                },
-                mapping = {
-                    -- if completion menu is visible, go to the next item
-                    -- else, trigger completion menu
-                    ['<C-n>'] = cmp.mapping(function()
-                        if cmp.visible() then
-                            cmp.select_next_item(select_opts)
-                        else
-                            cmp.complete()
-                        end
-                    end),
+			local cmp_config = {
+				completion = {
+					completeopt = "menu,menuone,noinsert",
+				},
+				snippet = {
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body)
+					end,
+				},
+				sources = {
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" },
+					{ name = "buffer" },
+					{ name = "path" },
+					{ name = "cmd" },
+				},
+				mapping = {
+					-- if completion menu is visible, go to the next item
+					-- else, trigger completion menu
+					["<C-n>"] = cmp.mapping(function()
+						if cmp.visible() then
+							cmp.select_next_item(select_opts)
+						else
+							cmp.complete()
+						end
+					end),
 
-                    -- if completion menu is visible, go to the previous item
-                    -- else, trigger completion menu
-                    ['<C-p>'] = cmp.mapping(function()
-                        if cmp.visible() then
-                            cmp.select_prev_item(select_opts)
-                        else
-                            cmp.complete()
-                        end
-                    end),
+					-- if completion menu is visible, go to the previous item
+					-- else, trigger completion menu
+					["<C-p>"] = cmp.mapping(function()
+						if cmp.visible() then
+							cmp.select_prev_item(select_opts)
+						else
+							cmp.complete()
+						end
+					end),
 
-                    -- scroll up and down in the completion documentation
-                    ['<C-u>'] = cmp.mapping.scroll_docs(-5),
-                    ['<C-d>'] = cmp.mapping.scroll_docs(5),
+					-- scroll up and down in the completion documentation
+					["<C-u>"] = cmp.mapping.scroll_docs(-5),
+					["<C-d>"] = cmp.mapping.scroll_docs(5),
 
-                    -- complete
-                    ["<C-Space>"] = cmp.mapping.complete(),
+					-- complete
+					["<C-Space>"] = cmp.mapping.complete(),
 
-                    -- cancel completion
-                    ['<C-e>'] = cmp.mapping.abort(),
+					-- cancel completion
+					["<C-e>"] = cmp.mapping.abort(),
 
-                    -- confirm selection
-                    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+					-- confirm selection
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
 
-                    -- navigate items on the list
-                    ['<Up>'] = cmp.mapping.select_prev_item(select_opts),
-                    ['<Down>'] = cmp.mapping.select_next_item(select_opts),
-                },
-                formatting = {
-                    fields = { 'abbr', 'menu', 'kind' },
-                    format = function(entry, item)
-                        local short_name = {
-                            nvim_lsp = 'LSP',
-                            nvim_lua = 'nvim'
-                        }
+					-- navigate items on the list
+					["<Up>"] = cmp.mapping.select_prev_item(select_opts),
+					["<Down>"] = cmp.mapping.select_next_item(select_opts),
+				},
+				formatting = {
+					fields = { "abbr", "menu", "kind" },
+					format = function(entry, item)
+						local short_name = {
+							nvim_lsp = "LSP",
+							nvim_lua = "nvim",
+						}
 
-                        local menu_name = short_name[entry.source.name] or entry.source.name
+						local menu_name = short_name[entry.source.name] or entry.source.name
 
-                        item.menu = string.format('[%s]', menu_name)
-                        return item
-                    end,
-                },
-            }
+						item.menu = string.format("[%s]", menu_name)
+						return item
+					end,
+				},
+			}
 
-            cmp.setup(cmp_config)
-        end
-    },
+			cmp.setup(cmp_config)
+
+			-- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+			cmp.setup.cmdline("/", {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = {
+					{ name = "buffer" },
+				},
+			})
+
+			-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+			cmp.setup.cmdline(":", {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = cmp.config.sources({
+					{ name = "path" },
+				}, {
+					{ name = "cmdline" },
+				}),
+			})
+		end,
+	},
+	{
+		"echasnovski/mini.pairs",
+		event = "VeryLazy",
+		opts = {}, -- this is equalent to setup({}) function
+	},
+	{
+		"echasnovski/mini.surround",
+		keys = function(_, keys)
+			-- Populate the keys based on the user's options
+			local plugin = require("lazy.core.config").spec.plugins["mini.surround"]
+			local opts = require("lazy.core.plugin").values(plugin, "opts", false)
+			local mappings = {
+				{ opts.mappings.add, desc = "Add surrounding", mode = { "n", "v" } },
+				{ opts.mappings.delete, desc = "Delete surrounding" },
+				{ opts.mappings.find, desc = "Find right surrounding" },
+				{ opts.mappings.find_left, desc = "Find left surrounding" },
+				{ opts.mappings.highlight, desc = "Highlight surrounding" },
+				{ opts.mappings.replace, desc = "Replace surrounding" },
+				{ opts.mappings.update_n_lines, desc = "Update `MiniSurround.config.n_lines`" },
+			}
+			mappings = vim.tbl_filter(function(m)
+				return m[1] and #m[1] > 0
+			end, mappings)
+			return vim.list_extend(mappings, keys)
+		end,
+		opts = {
+			mappings = {
+				add = "gza", -- Add surrounding in Normal and Visual modes
+				delete = "gzd", -- Delete surrounding
+				find = "gzf", -- Find surrounding (to the right)
+				find_left = "gzF", -- Find surrounding (to the left)
+				highlight = "gzh", -- Highlight surrounding
+				replace = "gzr", -- Replace surrounding
+				update_n_lines = "gzn", -- Update `n_lines`
+			},
+		},
+	},
 }
-
