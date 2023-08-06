ea91d70799f5191435ab8d9edf0181615e98bac2
CronyAkatsuki <crony@cronyakatsuki.xyz>
2023-08-06 16:00:11 +0200

    Update formatting

--- lua/crony/plugins/lang/python.lua
+++ lua/crony/plugins/lang/python.lua
@@ -1,49 +1,57 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        opts = function(_, opts)
-            if type(opts.ensure_installed) == "table" then
-                vim.list_extend(opts.ensure_installed, { "python" })
-            end
-        end,
-    },
-    {
-        -- Ensure C/C++ debugger and lsp are installed
-        "williamboman/mason.nvim",
-        optional = true,
-        opts = function(_, opts)
-            if type(opts.ensure_installed) == "table" then
-                vim.list_extend(opts.ensure_installed, { "pyright", "mypy", "ruff", "black", "debugpy" })
-            end
-        end,
-    },
-    {
-        "jose-elias-alvarez/null-ls.nvim",
-        ft = { "python" },
-        opts = function(_, opts)
-            local nls = require("null-ls")
-            if type(opts.sources) == "table" then
-                vim.list_extend(
-                    opts.sources,
-                    { nls.builtins.diagnostics.mypy, nls.builtins.diagnostics.ruff, nls.builtins.formatting.black }
-                )
-            end
-        end,
-    },
-    -- {
-    --     "mfussenegger/nvim-dap-python",
-    --     ft = "python",
-    --     config = function()
-    --         require("dap-python").setup('~/.local/share/nvim')
-    --     end,
-    --     keys = {
-    --         {
-    --             "<leader>dpr",
-    --             function()
-    --                 require("dap-python").test_method()
-    --             end,
-    --             desc = "Start python debuging",
-    --         },
-    --     },
-    -- },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = function(_, opts)
+			if type(opts.ensure_installed) == "table" then
+				vim.list_extend(opts.ensure_installed, { "python", "ninja", "rst", "toml" })
+			end
+		end,
+	},
+	{
+		-- Ensure C/C++ debugger and lsp are installed
+		"williamboman/mason.nvim",
+		optional = true,
+		opts = function(_, opts)
+			if type(opts.ensure_installed) == "table" then
+				vim.list_extend(opts.ensure_installed, { "pyright", "mypy", "ruff", "black", "debugpy" })
+			end
+		end,
+	},
+	{
+		"jay-babu/mason-nvim-dap.nvim",
+		opts = function(_, opts)
+			vim.list_extend(opts.ensure_installed, {
+				"python",
+			})
+		end,
+	},
+	{
+		"jose-elias-alvarez/null-ls.nvim",
+		ft = { "python" },
+		opts = function(_, opts)
+			local nls = require("null-ls")
+			if type(opts.sources) == "table" then
+				vim.list_extend(
+					opts.sources,
+					{ nls.builtins.diagnostics.mypy, nls.builtins.diagnostics.ruff, nls.builtins.formatting.black }
+				)
+			end
+		end,
+	},
+	{
+		"mfussenegger/nvim-dap",
+		optional = true,
+		dependencies = {
+			"mfussenegger/nvim-dap-python",
+            -- stylua: ignore
+            keys = {
+                { "<leader>dPt", function() require('dap-python').test_method() end, desc = "Debug Method" },
+                { "<leader>dPc", function() require('dap-python').test_class() end,  desc = "Debug Class" },
+            },
+			config = function()
+				local path = require("mason-registry").get_package("debugpy"):get_install_path()
+				require("dap-python").setup(path .. "/venv/bin/python")
+			end,
+		},
+	},
 }
