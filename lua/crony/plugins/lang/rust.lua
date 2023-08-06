afa290677ab79a4395734e7fc1d3dfb13636c8b3
CronyAkatsuki <crony@cronyakatsuki.xyz>
2023-08-06 15:59:15 +0200

    Added rust language support

---
+++ lua/crony/plugins/lang/rust.lua
@@ -0,0 +1,25 @@
+return {
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = function(_, opts)
+			if type(opts.ensure_installed) == "table" then
+				vim.list_extend(opts.ensure_installed, { "rust", "toml" })
+			end
+		end,
+	},
+	{
+		-- Ensure C/C++ debugger and lsp are installed
+		"williamboman/mason.nvim",
+		optional = true,
+		opts = function(_, opts)
+			if type(opts.ensure_installed) == "table" then
+				vim.list_extend(opts.ensure_installed, { "rust-analyzer" })
+			end
+		end,
+	},
+	{
+		"simrat39/rust-tools.nvim",
+        dependencies = "neovim/nvim-lspconfig",
+		ft = { "rust" },
+	},
+}
