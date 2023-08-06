21008f7bfaa8afe51e5f7d8d302d7e7aeb2805a4
CronyAkatsuki <crony@cronyakatsuki.xyz>
2023-08-06 16:01:53 +0200

    Update Formatting

--- lua/crony/lazy.lua
+++ lua/crony/lazy.lua
@@ -1,39 +1,40 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    spec = {
-        { import = "crony.plugins" },
-        { import = "crony.plugins.lang.clang" },
-        { import = "crony.plugins.lang.python" },
-    },
-    defaults = { lazy = true },
-    install = { colorscheme = { "catppuccin" } },
-    checker = { enabled = true },
-    performance = {
-        rtp = {
-            disabled_plugins = {
-                "gzip",
-                "matchit",
-                "matchparen",
-                "tar",
-                "tarPlugin",
-                "tohtml",
-                "tutor",
-                "fzf",
-                "zip",
-                "zipPlugin",
-            },
-        },
-    },
+	spec = {
+		{ import = "crony.plugins" },
+		{ import = "crony.plugins.lang.clang" },
+		{ import = "crony.plugins.lang.python" },
+		{ import = "crony.plugins.lang.rust" },
+	},
+	defaults = { lazy = true },
+	install = { colorscheme = { "catppuccin" } },
+	checker = { enabled = true },
+	performance = {
+		rtp = {
+			disabled_plugins = {
+				"gzip",
+				"matchit",
+				"matchparen",
+				"tar",
+				"tarPlugin",
+				"tohtml",
+				"tutor",
+				"fzf",
+				"zip",
+				"zipPlugin",
+			},
+		},
+	},
 })
